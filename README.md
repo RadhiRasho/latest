33499bc5fdcd1dffc80458a2dd464533ee111058
Radhi Rasho <Radhi.h@outlook.com>
2024-10-08 16:17:55 +0000

    published

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # bun-latest
 Inspired by [node-latest](https://github.com/bahamas10/node-latest) that I ran into without realizing and noticed it was dependent on npm, which wasn't really necessary so I found a way to use fetch and have similar functionality, plus some.
-
-Work in progress, will publish on npm when finished
--- latest.ts
+++ latest.ts
@@ -1,25 +1,39 @@
 #!/usr/bin/env node
 
-import { latest } from "./index.ts";
+import { latest } from "./index";
+
 
 const packages = process?.argv.slice(2);
 
 const versions = new Map<string, string>();
 let code = 0;
 
-for (const pkg of packages) {
-	const { value, error } = await latest(pkg);
+async function main() {
+	for (const pkg of packages) {
+		const { value, error } = await latest(pkg);
 
-	versions.set(pkg, value);
+		versions.set(pkg, value);
 
-	if (error) {
-		code++;
+		if (error) {
+			code++;
+		}
 	}
+
+	process.on("exit", () => {
+		for (const pkg of packages) {
+			console.log("%s: %s", pkg, versions.get(pkg));
+		}
+		process.exit(code);
+	});
 }
+
+main().catch((err) => {
+	console.error(err);
+	process.exit(1);
+});
 
 process.on("exit", async () => {
 	for (const pkg of packages) {
-		console.log("IT'sa me");
 		console.log("%s: %s", pkg, versions.get(pkg));
 	}
 	process.exit(code);
